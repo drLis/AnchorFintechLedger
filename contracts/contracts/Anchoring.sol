a8f581b896ccfcccad76c355d8356fe959c73278
dr.lis <dr.lis@outlook.com>
2021-02-20 09:27:13 +0300

    Fixed progress

--- contracts/contracts/Anchoring.sol
+++ contracts/contracts/Anchoring.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.11;
 
 contract Anchoring
 {
-	constructor()
+	constructor() public
 	{
 		issuer = msg.sender;
 	}
--- contracts/scripts/wallet.py
+++ contracts/scripts/wallet.py
@@ -1,6 +1,7 @@
 from brownie import *
 from brownie import web3
 import json
+import copy
 
 # Example of banknote
 banknote_example = {"info":{"nominal": 100, "id": 1, "issuer": accounts[0].address},
@@ -22,3 +23,19 @@ def issue_banknote(id, nominal, account_issuer, account_target):
 	banknote["indorsement"].append([account_target.address, web3.eth.sign(account_issuer.address, text = json.dumps(transfer))])
 
 	return banknote
+
+def transfer_banknote(banknote, receiver):
+	new_banknote = copy.deepcopy(banknote)
+
+	transfer = {'receiver': receiver.address, 'id': new_banknote["info"]["id"]}
+	owner = banknote["indorsement"][-1][0]
+	new_banknote["indorsement"].append([receiver.address, web3.eth.sign(owner, text = json.dumps(transfer))])
+
+	return new_banknote
+
+def validate_banknote(banknote):
+	result = False
+	anchoring = Anchoring[0]
+
+
+	return True
